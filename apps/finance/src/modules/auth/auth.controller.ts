d2cda927a43b9c0908e42b67bc266d8debcd22c9
sthaSandesh <sandeshstha561@gmail.com>
2025-09-16 18:45:58 +0545

    auth model signin signup

--- apps/finance/src/modules/auth/auth.controller.ts
+++ apps/finance/src/modules/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService : AuthService) {}
-    @Post('signin')
-    signin(){
-        return this.authService.signin()
-    }
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(){
-        return this.authService.signup()
-    }
-    
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
 }
-
--- apps/finance/src/modules/auth/auth.module.ts
+++ apps/finance/src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseModule } from '../../database/database.module';
 
 @Module({
+  imports:[DatabaseModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
